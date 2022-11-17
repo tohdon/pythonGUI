fc1210685acf97d4cf59b42931bfb9b34bf20c62
tohdon <74751540+tohdon@users.noreply.github.com>
2022-11-18 07:20:29 +0800

    Create hellow_world-b.py

---
+++ hellow_world-b.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+
+class HelloView(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.name = tk.StringVar()
+        self.hello_string = tk.StringVar()
+        self.hello_string.set("Hello World")
+
+        name_label = ttk.Label(self, text="Name:")
+        name_entry = ttk.Entry(self, textvariable=self.name)
+
+        ch_Button = ttk.Button(self, text="Change", command=self.on_change)
+        hello_label = ttk.Label(self, textvariable=self.hello_string, font=("TkDefaultFont", 64), wraplength=600)
+        name_label.grid(row=0, column=0, sticky=tk.W)
+        name_entry.grid(row=0, column=1, sticky=(tk.W + tk.E))
+        ch_Button.grid(row=0, column=2, sticky=tk.E)
+        hello_label.grid(row=1, column=0, columnspan=3)
+        self.columnconfigure(1, weight=1)
+        
+    def on_change(self):
+        if self.name.get().strip():
+            self.hello_string.set("Hello  " + self.name.get())
+        else:
+            self.hello_string.set("Hello World")
+            
+class MyApplication(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Hello Tkinter")
+        self.geometry("800x600")
+        self.resizable(width=False, height=False)
+        HelloView(self).grid(sticky=(tk.E + tk.W + tk.N + tk.S))
+        self.columnconfigure(0, weight=1)
+    
+    
+if __name__ == '__main__':
+    app = MyApplication()
+    app.mainloop()
